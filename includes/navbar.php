7c2b0962f37b1c5335b90b08edf14c6d31d1a4cb
Konrad <wi24b078@technikum-wien.at>
2025-10-28 15:46:26 +0100

    navbarphp added and simplified

---
+++ includes/navbar.php
@@ -0,0 +1,46 @@
+  <!--requests current page-->
+  <?php
+    $currentPage = $_SERVER['REQUEST_URI'];
+  ?>
+
+  <!--nav bar-->
+    <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand fw-bold" href="index.php">StudyWithMe</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link <?php if (basename($currentPage) === 'createAccount.php') echo 'active'; ?>" 
+            href="createAccount.php">Join Now</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if (basename($currentPage) === 'login.php') echo 'active'; ?>" 
+            href="login.php">Log In</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if (basename($currentPage) === 'index.php') echo 'active'; ?>"
+            href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if (basename($currentPage) === 'campusmap.php') echo 'active'; ?>"
+            href="campusmap.php">Campus-Map</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
