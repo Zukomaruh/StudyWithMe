f21fbd11f1e7886ff7104cd00a5d03c9e23f7550
Konrad <wi24b078@technikum-wien.at>
2025-10-31 11:47:12 +0100

    rooms ind construction

--- rooms.php
+++ rooms.php
@@ -9,19 +9,46 @@
 <?php
 // Gebäude aus URL-Parameter lesen (Standard: F)
 $currentBuilding = isset($_GET['building']) ? strtoupper($_GET['building']) : 'F';
+
+// funktioniert nur wenn wir Raumselection als Formular machen (zB Dropdown oä):
+$selectedFloor = $_POST['floor'] ?? 0; 
 ?>
 <div class="container-fluid text-center py-4">
   <div class="row g-4">
     <!-- Jede col nimmt auf kleinen Displays 12 Spalten (volle Breite), 
          und ab mittleren Displays 6 Spalten (also 2 nebeneinander) -->
     <div class="col-12 col-md-6">
-      <div class="p-3 bg-light border h-100">Image Raumplan</div>
+      <div class="p-3 bg-light border h-100">
+        <img
+        src="assets/img/FloorPlans/<?php echo $currentBuilding.$selectedFloor; ?>.png"
+        alt="Floor Plan"
+        class="img-fluid mx-auto d-block"
+        style="max-width: 100%; height: auto;"
+      />
+      </div>
     </div>
     <div class="col-12 col-md-6">
-      <div class="p-3 bg-light border h-100">Room Information</div>
+      <div class="p-3 bg-light border h-100">
+        <h3>Room Information</h3>
+        <?php if (empty($selectedRoom)) : ?>
+          <p>please pick a room...</p>
+        <?php endif; ?>
+      </div>
     </div>
     <div class="col-12 col-md-6">
-      <div class="p-3 bg-light border h-100">Choose Floor</div>
+      <div class="p-3 bg-light border h-100"><div class="d-flex flex-column align-items-center">
+        <h5 class="mb-3">Pick a floor</h5>
+        <div class="d-flex bg-success text-white rounded-pill p-1">
+        <!--Muss noch angepasst werden mit php dass je nach Building andere Floors gezeigt werden sonst hauts hin-->
+            <form method="post">
+                <button type="submit" name="floor" value="0" class="btn btn-success rounded-pill mx-1">UG</button>
+                <button type="submit" name="floor" value="1" class="btn btn-success rounded-pill mx-1">1</button>
+                <button type="submit" name="floor" value="2" class="btn btn-success rounded-pill mx-1">2</button>
+                <button type="submit" name="floor" value="4" class="btn btn-success rounded-pill mx-1">4</button>
+            </form>
+            <?php // echo $selectedFloor; ?>
+        </div>
+    </div></div>
     </div>
     <div class="col-12 col-md-6">
       <div class="p-3 bg-light border h-100">Choose Room</div>
