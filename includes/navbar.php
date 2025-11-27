e8a82dddc1f24b5b27c49e824c94bcca8b1466c8
Konrad <wi24b078@technikum-wien.at>
2025-11-27 12:41:09 +0100

    bugfix

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,8 @@
   <!--requests current page-->
   <?php
-    session_start();
+    if(session_status() === PHP_SESSION_NONE){
+      session_start();
+    }
     $currentPage = basename($_SERVER['REQUEST_URI']);
   ?>
 
